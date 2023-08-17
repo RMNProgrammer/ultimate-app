a199ff45e6ad3aeb56505ef1c2fb1e57d1277718
RMNProgrammer <mohammadaminrmn.programmer@gmail.com>
2023-08-17 13:12:04 +0430

    Use process.env to increase security

--- utils/fetchUrl.js
+++ utils/fetchUrl.js
@@ -1,12 +1,13 @@
 const fetchUrl = async ({ url, method = 'GET' }) => {
-    return new Promise((resolve) => {
-        setTimeout( async () => {
-                const response = await fetch(url,{ 
-                    method 
-                })
-                resolve(await response.json())
-        }, 5000); 
-    })
+   const absoluteUrl = `${process.env.NEXT_PUBLIC_ABSOLUTE_URL}${url}`
+   return new Promise((resolve) => {
+      setTimeout( async () => {
+         const response = await fetch(absoluteUrl,{ 
+            method 
+         })
+         resolve(await response.json())
+      }, 5000); 
+   })
 }
 
 export default fetchUrl
